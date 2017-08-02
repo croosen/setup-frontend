dd97809ea32ab9dd8f3782620a2af194d46134f2
Corinne Roosen <croosen@users.noreply.github.com>
2017-08-02 17:01:57 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <?php include 'components/card.tpl.php'; ?>
+        Hello you!
 
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
